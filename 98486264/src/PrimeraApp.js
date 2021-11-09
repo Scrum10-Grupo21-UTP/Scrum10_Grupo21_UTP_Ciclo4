567c94bbbab5acf79581418239c8c97e025e0df2
robertrb63 <81638977+robertrb63@users.noreply.github.com>
2021-11-09 07:46:35 -0500

    Add files via upload

---
+++ 98486264/src/PrimeraApp.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+// import React, { Fragment } from 'react';
+
+const PrimeraApp = ({ saludo, subtitulo }) => {
+    const titulo = 'hola soy un saludo';
+    
+    return (
+        <>
+
+            <h1>{ titulo }!!!</h1>
+            <p>{subtitulo}</p>
+
+
+        </>
+    );
+        
+} 
+
+PrimeraApp.propTypes = {
+    saludo: PropTypes.string.isRequired
+}
+
+PrimeraApp.defaultProps = {
+    subtitulo: 'Soy un subtitulo'
+}
+
+
+export default PrimeraApp;
---
+++ 98486264/src/tests/PrimeraApp.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import '@testing-library/jest-dom'
+import { shallow } from 'enzyme'
+
+import PrimeraApp from '../PrimeraApp';
+
+
+describe('Pruebas en <PrimeraApp />', () => {
+    
+    test('debe de mostrar <PrimeraApp /> correctamente', () => {
+
+        const saludo = 'Hola, Soy Goku';
+        const wrapper = shallow( <PrimeraApp saludo={ saludo } /> );
+
+        expect( wrapper ).toMatchSnapshot();
+        
+    });
+
+    test('debe de mostrar el subtitulo enviado por props', () => {
+        
+        const saludo = 'Hola, soy un saludo';
+        const subTitulo = 'Soy un subtitulo';
+
+        const wrapper = shallow( 
+            <PrimeraApp 
+                saludo={ saludo }
+                subtitulo={ subTitulo }
+            /> 
+        );
+
+        const textoParrafo = wrapper.find('p').text();
+        
+        expect( textoParrafo ).toBe( subTitulo );
+
+    })  
+
+})
